ff79da7fd6f6cf5a511172191d55084a0f677fc5
Raul Larriega <raulgf_02@hotmail.com>
2018-09-01 17:41:59 -0500

    Funciones puras

--- funciones-puras.js
+++ funciones-puras.js
@@ -6,8 +6,40 @@ function pureSuma(a,b){
 }
 
 console.log(pureSuma(1,2));
-
 // Impura
 const time = () => Date.now();
 console.log(time());
 console.log(time());
+//Otro ejemplo de funcion impura
+function sumador(suma=0){
+	return function(cantidad){
+		suma = suma + cantidad;
+		return suma;
+	}
+}
+const sumx = sumador();
+console.log(sumx(2));// 2
+console.log(sumx(2));// 4
+
+//Convirtiendo la funcion sumador a pura
+function sumadorPure(suma=0){
+	return function(cantidad){
+		return suma + cantidad;
+	}
+}
+const sump = sumadorPure();
+console.log(sump(2));// 2
+console.log(sump(2));// 2
+
+// funcion impura
+var min = 10;
+function comparaMinimo(num){
+	return num < min;
+}
+// Esta funcion es impura ya que depende de un estado externo
+
+//Funcion pura
+function comparaMinimo(num){
+	var min = 10;
+	return num < min;
+}
