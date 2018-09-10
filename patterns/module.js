52ec15aa6727354d5c8c612557feb988d207f648
Raul Larriega <raulgf_02@hotmail.com>
2018-09-09 23:16:50 -0500

    Patrones de diseño +1

--- patterns/module.js
+++ patterns/module.js
@@ -5,13 +5,16 @@ myModule;
 //Similar a la construccion de un JSON 
 let calculadora = (() => {
     let publicFunction = {};
+    //Funcion privada :) 
     privateFunction = () => '439294392';
 
     publicFunction.suma = (a,b) => {
         let result = a + b;
         console.log(`El resultado de la suma de ${a} y ${b} es ${result}`);
     };
+    publicFunction.getPrivateFunction = () => console.log(`Valor de privateFunction es ${privateFunction()}`);
     return publicFunction;
 })();
 calculadora.suma(4,3);
-calculadora.privateFunction();// no se puede acceder a esta funcion
+//calculadora.privateFunction();// no se puede acceder a esta funcion
+calculadora.getPrivateFunction();
