a157a77f062f4c63e7add64dc7a93a0952c5fe0b
Larrygf02 <rlarriega@esan.edu.pe>
2019-04-01 16:02:55 -0500

    Name and parameters the function

---
+++ function.js
@@ -0,0 +1,13 @@
+function saludo() {
+    console.log('hola')
+}
+
+console.log(saludo.name);
+
+function hola(name){
+    console.log(`Hola ${name}`);
+}
+
+//cuenta el numero de parametros de la funcion
+console.log(saludo.length);
+console.log(hola.length);
