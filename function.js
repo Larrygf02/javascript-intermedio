ee7b8ab97f01f2de534c2c8685c6bbf952896d8a
Larrygf02 <rlarriega@esan.edu.pe>
2019-04-01 16:19:52 -0500

    Contar cuantas veces ha sido ejecutado una funcion

--- function.js
+++ function.js
@@ -6,8 +6,13 @@ console.log(saludo.name);
 
 function hola(name){
     console.log(`Hola ${name}`);
+    hola.counter++;
 }
-
+hola.counter = 0;
 //cuenta el numero de parametros de la funcion
 console.log(saludo.length);
 console.log(hola.length);
+hola('kelly');
+hola('raul');
+
+console.log(`la funcion ${hola.name} a sido ejecutada ${hola.counter}`);
