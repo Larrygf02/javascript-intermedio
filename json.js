e8a29acacee5e467d762d231e50573c0a6158157
Larrygf02 <raulgf_02@hotmail.com>
2019-04-11 15:50:10 -0500

    JSON methods

--- json.js
+++ json.js
@@ -7,3 +7,31 @@ let student = {
 let json = JSON.stringify(student)
 
 console.log(json);
+//El segundo parametro indica las propiedades que quieres parsear
+let json_student = JSON.stringify(student, ['age', 'name'],2)
+console.log(json_student);
+
+let user = {
+    sayHi() {
+        console.log('hola');
+    },
+    [Symbol("id")]: 123,
+    something: undefined
+};
+
+let user_json = JSON.stringify(user)
+
+console.log(user);
+
+let room = {
+    number: 23
+}
+
+let meetup = {
+    title: 'Conference'
+}
+
+meetup.place = room
+room.occupied = meetup
+// console.log(JSON.stringify(meetup)); -> Esto saldrá un error de converting circular
+
