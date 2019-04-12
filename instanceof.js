af4c584faf4e5ba1312a82e423280f9631d68a77
Larrygf02 <raulgf_02@hotmail.com>
2019-04-12 11:51:33 -0500

    ToString

--- instanceof.js
+++ instanceof.js
@@ -23,3 +23,10 @@ let obj = { teaches: true }
 let obj2 = { other: true }
 console.log(obj instanceof Teacher);
 console.log(obj2 instanceof Teacher);
+
+//ToString
+let s = Object.prototype.toString
+
+console.log(`123: ${s.call(123)}`);
+console.log(`null: ${s.call(null)}`);
+//console.log(`alert: ${s.call(alert)}`);
